29ba440e6d4f7455758a9d4f056345c125034596
arnab-api <arnab.api@gmail.com>
2024-03-15 23:18:49 -0400

    Mamba Edit >> Added support to change the later_half slice of in_proj matrix -- basically it will only effect the residual/MLP dataflow and leave the ssm dataflow alone

--- src/rome/rome_main.py
+++ src/rome/rome_main.py
@@ -11,6 +11,7 @@ from src.functional import free_gpu_cache
 from src.models import ModelandTokenizer
 from src.rome_utils import nethook
 from src.utils.generation import generate_fast
+from src.models import determine_hidden_size
 
 from .compute_u import compute_u
 from .compute_v import compute_v
@@ -114,6 +115,8 @@ def apply_rome_to_model(
         with torch.no_grad():
             w_name, (delta_k, delta_v) = list(deltas.items())[0]
             weights = nethook.get_parameter(mt.model, w_name)
+            if hparams.mamba_block_residual:
+                weights = weights[weights.shape[0] // 2 :]
             upd_matrix = delta_k.unsqueeze(1) @ delta_v.unsqueeze(0)
             upd_matrix = upd_matrix_match_shape(upd_matrix, weights.shape)
             weights[...] += upd_matrix
